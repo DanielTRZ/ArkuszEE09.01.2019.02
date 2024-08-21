868edd5532fa322115b703cd1980beb8c3080eff
Daniel Trzeciński <71105883+DanielTRZ@users.noreply.github.com>
2024-08-21 19:07:50 +0200

    Update java.js

--- java.js
+++ java.js
@@ -12,7 +12,7 @@ var kursy = document.getElementById("kursywa").value;
 if(kursy.onchange="format();"){      
 document.getElementById('para').style.fontStyle = "italic";
 } else { prost.onchange="format();"
-        document.getElementById('para').style.fontStyle = "normal";
+document.getElementById('para').style.fontStyle = "normal";
         
     }   
       
